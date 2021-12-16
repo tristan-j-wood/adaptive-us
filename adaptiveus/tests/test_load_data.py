21cb89a9fd1b46f9501c10020491da1278feddc7
Tristan Johnston-Wood <tristan.johnston-wood@chem.ox.ac.uk>
2021-12-16 15:27:40 +0000

    Creating setup files

---
+++ adaptiveus/tests/test_load_data.py
@@ -0,0 +1,6 @@
+import adaptiveus as adp
+
+
+def test_load_data():
+
+    adaptive = adp.adaptive.Window()
--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='adaptive-us',
-    version='1.0.0',
-    packages=[''],
-    url='',
-    license='',
-    author='tristanjwood',
+    name='adaptiveus',
+    version='1.0.0a1',
+    description='Adaptive sampling for umbrella sampling',
+    packages=['adaptiveus'],
+    url='https://github.com/tristan-j-wood/adaptive-us',
+    license='MIT',
+    author='Tristan Johnston-Wood',
     author_email='tjohnstonwood@gmail.com',
-    description=''
 )
