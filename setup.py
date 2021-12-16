607c8f7f3c49d59d9759b48fd00ce010680c610e
Tristan Johnston-Wood <tristan.johnston-wood@chem.ox.ac.uk>
2021-12-16 14:05:27 +0000

    Creating base files for tests and main

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='adaptive-us',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='',
+    author='tristanjwood',
+    author_email='tjohnstonwood@gmail.com',
+    description=''
+)
