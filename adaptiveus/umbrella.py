7b6e96028173b9e640b39ad6c30a127e7da77cbe
Tristan Johnston-Wood <tristan.johnston-wood@chem.ox.ac.uk>
2022-01-10 11:26:03 +0000

    Adaptive functions made general to any MD driver
    Started implementation of mltrain MD driver

--- adaptiveus/umbrella.py
+++ adaptiveus/umbrella.py
@@ -1,4 +1,3 @@
-import numpy as np
 from mltrain.sampling.umbrella import UmbrellaSampling
 from adaptiveus.adaptive import Windows
 from typing import Optional, Callable
@@ -8,45 +7,70 @@ class Adaptive:
 
     def __init__(self,
                  method: str,
+                 mlp: Optional['mltrain.potentials._base.MLPotential'],
                  zeta_func: 'mltrain.sampling.reaction_coord.ReactionCoordinate',
-                 kappa: float = 20):
+                 kappa: float,
+                 temp: float,
+                 interval: int,
+                 dt: float):
         """
         Perform adaptive umbrella sampling using (currently only) mltrain to
         drive the dynamics
 
         -----------------------------------------------------------------------
         Arguments:
-            method: Name of umbrella sampling driver: 'mltrain', 'gmx'
+            method: Name of umbrella sampling driver: 'mlt', 'gmx'
+
+            mlp: Machine learnt potential
 
             zeta_func: Reaction coordinate, as the function of atomic positions
 
             kappa: Value of the spring constant, κ, used in umbrella sampling
+
+            temp: Temperature in K to initialise velocities and to run NVT MD.
+                  Must be positive
+
+            interval: (int) Interval between saving the geometry
+
+            dt: (float) Time-step in fs
         """
 
         self.method = method
+        self.mlp = mlp
         # The zeta function is currently set up for mltrain only
         self.zeta_func:         Callable = zeta_func
         self.kappa = kappa
 
-    def _run_single_window(self, ref, config):
+        self.temp = temp
+        self.interval = interval
+        self.dt = dt
+
+        self.windows = Windows()
+
+    def _run_single_window(self, ref, traj, idx, **kwargs) -> None:
+        """Run a single umbrella sampling window using a specified method"""
 
         if self.method == 'mlt':
-            adaptive = MltrainAdaptive()
-            adaptive.run_mlt_adaptive()
+            adaptive = MltrainAdaptive(zeta_func=self.zeta_func,
+                                       kappa=self.kappa,
+                                       temp=self.temp,
+                                       interval=self.interval,
+                                       dt=self.dt)
 
-            # Get data and stuff from running these and then process?
+            adaptive.run_mlt_window(traj=traj, mlp=self.mlp, ref=ref, idx=idx,
+                                    **kwargs)
 
-        return NotImplementedError
+            self.windows.load(f'window_{idx}.txt')
+
+        else:
+            raise NotImplementedError
+
+        return None
 
     def run_adaptive(self,
-                     traj: Optional,  # Coordinate along the reaction coord
-                     mlp: Optional['mltrain.potentials._base.MLPotential'],
-                     temp: float,
-                     interval: int,
-                     dt: float,
+                     traj: Optional,
                      init_ref: Optional[float] = None,
                      final_ref: Optional[float] = None,
-                     n_windows: Optional[int] = 10,
                      **kwargs):
         """
         Run adaptive umbrella sampling for ml-train
@@ -57,22 +81,11 @@ class Adaptive:
                   a 'pulling' trajectory that has sufficient sampling of a
                   range f reaction coordinates
 
-            mlp: Machine learnt potential
-
-            temp: Temperature in K to initialise velocities and to run NVT MD.
-                  Must be positive
-
-            interval: Interval between saving the geometry
-
-            dt: Time-step in fs
-
             init_ref: Value of reaction coordinate in Å for
                       first window
 
             final_ref: Value of reaction coordinate in Å for
                        first window
-
-            n_windows: Number of windows to run in the umbrella sampling
 
         -------------------
         Keyword Arguments:
@@ -83,16 +96,36 @@ class Adaptive:
         if init_ref is None:
             init_ref = self.zeta_func(traj[0])
 
-        self._run_single_window(init_ref, traj)
+        self._run_single_window(init_ref, traj, idx=0, **kwargs)
+
+        # disc = self.windows[0].discrepancy
+
+        # adaptive.calculate_overlap([idx, idx+1])
+        #
+        # adaptive.plot_histogram()
+        # adaptive.plot_overlaps()
 
         # Run 1 window and check stuff. Choose k and then run next window
+
+        # while ref < final_ref:
+        #
+        #     self._run_single_window(new_ref, traj)
+        #
+        #     calculate_overlap(idx_0, idx_1)
+        #
+        #     calculate_d()
+        #
+        #     calculate_new_k_and_ref()
 
 
 class MltrainAdaptive:
 
     def __init__(self,
                  zeta_func: 'mltrain.sampling.reaction_coord.ReactionCoordinate',
-                 kappa: float = 20):
+                 kappa: float,
+                 temp: float,
+                 interval: int,
+                 dt: float):
         """
         Perform adaptive umbrella sampling using mltrain to drive the dynamics
 
@@ -101,71 +134,26 @@ class MltrainAdaptive:
             zeta_func: Reaction coordinate, as the function of atomic positions
 
             kappa: Value of the spring constant, κ, used in umbrella sampling
+
+            temp: Temperature in K to initialise velocities and to run NVT MD.
+                  Must be positive
+
+            interval: (int) Interval between saving the geometry
+
+            dt: (float) Time-step in fs
         """
 
         self.kappa = kappa
         self.zeta_func:         Callable = zeta_func
 
-    def _run_umbrella_window(self, traj, mlp, temp, interval, dt, ref, idx,
-                             **kwargs) -> None:
+        self.temp = temp
+        self.interval = interval
+        self.dt = dt
+
+    def run_mlt_window(self, traj, mlp, ref, idx, **kwargs) -> None:
         """
         Run a single umbrella window using mltrain and save the sampled
         reaction coordinates
-        """
-
-        umbrella = UmbrellaSampling(zeta_func=self.zeta_func,
-                                    kappa=self.kappa)
-
-        umbrella.run_umbrella_sampling(traj=traj,
-                                       mlp=mlp,
-                                       temp=temp,
-                                       interval=interval,
-                                       dt=dt,
-                                       init_ref=ref,
-                                       final_ref=ref,
-                                       n_windows=1,
-                                       **kwargs)
-
-        umbrella.windows[0].save(f'window_{idx}.txt')
-
-        return None
-
-    @staticmethod
-    def add_window_idx_to_file(idx) -> None:
-        """Add the window index to the start of the reaction coordinate file"""
-
-        file_lines = open(f'window_{idx}.txt', 'r').readlines()
-        file_lines[0] = f'{idx} {file_lines[0]}'
-
-        with open(f'window_{idx}.txt', 'w') as outfile:
-            for line in file_lines:
-                print(line.strip('\n'), file=outfile)
-
-        return None
-
-    def _reference_values(self, traj, num, init_ref, final_ref) -> np.ndarray:
-        """Return the set of reference values across the reaction coordinate"""
-
-        if init_ref is None:
-            init_ref = self.zeta_func(traj[0])
-
-        if final_ref is None:
-            final_ref = self.zeta_func(traj[-1])
-
-        return np.linspace(init_ref, final_ref, num)
-
-    def run_mlt_adaptive(self,
-                         traj: 'mltrain.ConfigurationSet',
-                         mlp: 'mltrain.potentials._base.MLPotential',
-                         temp: float,
-                         interval: int,
-                         dt: float,
-                         init_ref: Optional[float] = None,
-                         final_ref: Optional[float] = None,
-                         n_windows: int = 10,
-                         **kwargs) -> None:
-        """
-        Run adaptive umbrella sampling for ml-train
 
         -----------------------------------------------------------------------
         Arguments:
@@ -175,20 +163,9 @@ class MltrainAdaptive:
 
             mlp: Machine learnt potential
 
-            temp: Temperature in K to initialise velocities and to run NVT MD.
-                  Must be positive
+            ref: Reference value for the harmonic bias
 
-            interval: (int) Interval between saving the geometry
-
-            dt: (float) Time-step in fs
-
-            init_ref: (float | None) Value of reaction coordinate in Å for
-                       first window
-
-            final_ref: (float | None) Value of reaction coordinate in Å for
-                       first window
-
-            n_windows: (int) Number of windows to run in the umbrella sampling
+            idx: Index for the umbrella sampling window
 
         -------------------
         Keyword Arguments:
@@ -196,32 +173,34 @@ class MltrainAdaptive:
             {fs, ps, ns}: Simulation time in some units
         """
 
-        ref_values = self._reference_values(traj, n_windows,
-                                            init_ref, final_ref)
+        umbrella = UmbrellaSampling(zeta_func=self.zeta_func,
+                                    kappa=self.kappa)
 
-        window_idxs = []
-        for idx, ref in enumerate(ref_values):
-            self._run_umbrella_window(traj=traj,
-                                      mlp=mlp,
-                                      temp=temp,
-                                      interval=interval,
-                                      dt=dt,
-                                      ref=ref,
-                                      idx=idx,
-                                      **kwargs)
-            window_idxs.append(idx)
+        umbrella.run_umbrella_sampling(traj=traj,
+                                       mlp=mlp,
+                                       temp=self.temp,
+                                       interval=self.interval,
+                                       dt=self.dt,
+                                       init_ref=ref,
+                                       final_ref=ref,
+                                       n_windows=1,
+                                       **kwargs)
 
-        adaptive = Windows()
+        umbrella.windows[0].save(f'window_{idx}.txt')
 
-        for idx in window_idxs:
-            self.add_window_idx_to_file(idx)
-            # Save to a zip file
-            adaptive.load(f'window_{idx}.txt')
+        self._add_window_idx_to_file(idx)
 
-        for idx in window_idxs[:-1]:
-            adaptive.calculate_overlap([idx, idx+1])
+        return None
 
-        adaptive.plot_histogram()
-        adaptive.plot_overlaps()
+    @staticmethod
+    def _add_window_idx_to_file(idx) -> None:
+        """Add the window index to the start of the reaction coordinate file"""
+
+        file_lines = open(f'window_{idx}.txt', 'r').readlines()
+        file_lines[0] = f'{idx} {file_lines[0]}'
+
+        with open(f'window_{idx}.txt', 'w') as outfile:
+            for line in file_lines:
+                print(line.strip('\n'), file=outfile)
 
         return None
